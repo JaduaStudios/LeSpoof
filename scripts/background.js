3c08bb18c7396fafab77acad1dd5a959ff7a049c
Jadua1010 <jmb-n@outlook.com>
2024-05-13 19:08:36 +0200

    Updated versions for better support

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,4 @@
-localStorage["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76";
+localStorage["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
 
 (function () {
 	if(!localStorage["lastUpdate"] || Date.now() - localStorage["lastUpdate"] > 86400000){
